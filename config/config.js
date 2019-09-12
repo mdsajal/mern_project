b10fff7b9c1a937da27820bfb8e7eaf3ddf38782
MD Sajal Deowan <39679806+mdsajaldeowan1993@users.noreply.github.com>
2019-09-12 23:14:53 +0600

    rename mongodb uri

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 // module.exports = {
-// 	URI:
-// 		'mongodb://sajaldeowan:mlabpassword7659@ds151354.mlab.com:51354/mern_project',
+// 	URI:'mongodb URI',
 // 	PORT: process.env.PORT || 5000,
 // 	SECRET_KEY: 'SUPER_SECRET',
 // 	ACTIVE_LINK: 'http://localhost:5000/api/users/active/'
